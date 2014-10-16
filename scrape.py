495c9a16628b1027637bce4a9041ff8ba4784a50
Daniel Flint <def6054@rit.edu>
2014-10-16 00:25:23 -0400

    zipped to dictionary

--- scrape.py
+++ scrape.py
@@ -3,9 +3,19 @@ import requests
 
 page = requests.get('http://www.google.com/finance?q=f&ei=N4U9VIivJ4ebrAHP6oDYDQ')
 tree = html.fromstring(page.text)
-
 info = tree.xpath('//td[@class="val"]/text()')
+labels = ["Range",
+          "52 week",
+          "Open",
+          "Vol / Avg.",
+          "Mkt cap",
+          "P/E",
+          "Div/yield",
+          "EPS",
+          "Shares",
+          "Beta",
+          "Inst. own"]
 
+dictionary = dict(zip(labels, info))
 
-print 'Info: ', info
-
+print float(dictionary["Beta"]) * float( dictionary["Range"][0:dictionary["Range"].index(" ")] )
